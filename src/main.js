312274e9059150907ac0eb263154412040972061
imskanand <the.anand3099@gmail.com>
2023-02-17 20:28:06 +0530

    Add : Console Methods of Js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+console.log("Hello world!")
+
+console.log(10, 'imskanand', true)
+
+console.table({ name: 'shubham', email: 'imskana@nd.com' })
+
+console.group('Attributes')
+console.log(21)
+console.warn('warning')
+console.error('error')
+console.groupEnd()
+
+const style = 'padding:10px; background-color:green; text:white;'
+console.log('%c Shubham Kumar Anand', style)
