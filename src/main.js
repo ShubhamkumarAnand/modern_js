e9f073cdb0dde56cdd88b2f46e605ab65bca498a
imskanand <the.anand3099@gmail.com>
2023-02-18 00:13:33 +0530

    Add: stack & Heap

--- src/main.js
+++ src/main.js
@@ -1,30 +1,14 @@
-// null
-const aptNumber = null
-console.log(aptNumber, typeof aptNumber)
-
-// undefined
-let minSal;
-console.log(minSal, typeof minSal)
-
-const maxSal = undefined
-console.log(maxSal, typeof maxSal)
-
-// symbol
-const symbol = Symbol('int')
-console.log(symbol, typeof symbol)
-
-// Reference type
-const arr = [1,2,3,4]
-console.log(arr, typeof arr)
+// Primitive Data Types Stored on the Stack
+// Non-Primitive Data Types Stored on the Heap with the reference to the stack
 
 const person = {
   name: 'John',
-  age: 21
+  email: 'john@example.com'
 }
 
-console.log(person, typeof person)
+console.log(person)
+const person2 = person
 
-function sayHello() {
-  console.log('Hello')
-}
-console.log(sayHello, typeof sayHello)
+person2.name = 'John Smith'
+console.log(person2)
+console.log(person)
