73563e191984515c4113435c25fabce51c2049c7
imskanand <the.anand3099@gmail.com>
2023-03-03 13:12:19 +0530

    String and methods in Js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-// Type Coercion
+// String
 
-let x = 5 + '5';
-x = 'a' * 'a'
-x = '5' * '5'
-x = Number(null)
-x = undefined + undefined
-x = null + undefined
-console.log(x, typeof x);
+const s = new String("imskanand How are you?");
+let x = s.__proto__;
+
+x = s.includes('Hello')
+x = s.substring(1, 4)
+x = s.slice(-1)
+x = s.valueOf()
+x = s.split(' ')
+console.log(x)
