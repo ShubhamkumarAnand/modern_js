4cfbc71833d4c38773d0644cdc94ae07abbd59f5
imskanand <the.anand3099@gmail.com>
2023-03-03 17:14:06 +0530

    string challenge in Js

--- src/main.js
+++ src/main.js
@@ -9,3 +9,10 @@ x = s.slice(-1)
 x = s.valueOf()
 x = s.split(' ')
 console.log(x)
+
+// challenge
+
+let myRole = 'developer'
+myRole = myRole.charAt(0).toUpperCase() + myRole.substring(1)
+myRole = myRole.charAt(0).toUpperCase() + myRole.slice(1,myRole.length)
+console.log(myRole)
